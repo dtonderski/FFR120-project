62546e7c8897120d9b58e7a72e80d3888f970d95
ruxin-z <74582790+ruxin-z@users.noreply.github.com>
2020-11-23 16:47:19 +0800

    Update main.m

--- main.m
+++ main.m
@@ -1,35 +1,68 @@
 clear all
 clc
-addpath('classes/bug-model')
-addpath('classes/house-model')
+% addpath('classes/bug-model')
+% addpath('classes/house-model')
 AREA_SIZE = 10;
 
-numberOfSteps = 1000;
-numberOfRandomSteps = 50;
+numberOfBugs = 1;
+time = 1;
+numberOfTime = 100;
+numberOfSteps = 30;
+numberOfRandomSteps = 30;
 currentPositionX = zeros(numberOfSteps,1);
 currentPositionY = zeros(numberOfSteps,1);
 
 initialPosition = randi(AREA_SIZE,1,2);
-currentPositionX(1) = initialPosition(1);
-currentPositionY(1) = initialPosition(2);
+X = [];
+Y = [];
 
-bug1 = Bug(currentPositionX(1), currentPositionY(1), AREA_SIZE);
-figure();
-axis([0.5 AREA_SIZE+0.5 0.5 AREA_SIZE+0.5]);
-h = line('XData',[],'YData',[],'color','b','marker','.','markersize',30);
-set(h,'XData',bug1.row,'YData',bug1.col);
+bug(1) = Bug(initialPosition(1), initialPosition(1), AREA_SIZE);
+X = [X;bug(1).row];
+Y = [Y;bug(1).col];
 
-validStandard = 1;
-i = 1;
-while (~isempty(validStandard) && i < numberOfSteps)
-    if (i <= numberOfRandomSteps)
-        bug1 = bug1.move(validStandard);
-    else
+figure(1)
+hold on
+plot(X,Y,'.')
+axis([0,AREA_SIZE,0,AREA_SIZE])
+axis equal square
+box on
+title(['Simulation step = ' num2str(time)])
+xlabel('X')
+ylabel('Y')
+
+
+while (time <= numberOfTime)
+
+    validStandard = 1;    
+    numberOfBugs = numel(bug);
+    for nBug = 1:numberOfBugs
         validStandard = input('Whether the place is valid or not? ');
-        bug1 = bug1.move(validStandard);
+        bug(nBug) = bug(nBug).move(validStandard);
+        X(nBug) = bug(nBug).row;
+        Y(nBug) = bug(nBug).col;
+        bug(nBug) = bug(nBug).grow();
+        if (bug(nBug).age == 3)
+            bug(numberOfBugs+1) = Bug(bug(numberOfBugs).row,bug(numberOfBugs).col,AREA_SIZE);
+            X = [X;bug(numberOfBugs+1).row];
+            Y = [Y;bug(numberOfBugs+1).col];
+        end
+        
+        if (bug(nBug).age >= 10)
+            bug(nBug) = bug(nBug).die;
+        end
     end
-    set(h,'XData',bug1.row,'YData',bug1.col);
-    drawnow
-    pause(0.1)
-    i = i+1;
+    
+    X = mod(X,AREA_SIZE);
+    Y = mod(Y,AREA_SIZE);    
+    
+    % Plot
+    if rem(time,1)==0
+        cla
+        plot(X,Y,'o')
+        title(['Simulation step = ' num2str(time)])
+        axis([0,AREA_SIZE,0,AREA_SIZE])
+        drawnow update
+    end
+    
+    time = time + 1;
 end
