8fc9347bb4b5d5ed0c1ec9ecfbdad316599083c1
ruxin-z <74582790+ruxin-z@users.noreply.github.com>
2020-11-23 16:44:55 +0800

    Update Bug.m

--- classes/bug-model/Bug.m
+++ classes/bug-model/Bug.m
@@ -1,45 +1,56 @@
 classdef Bug
-   properties
-      row {mustBeNumeric}
-      col {mustBeNumeric}
-	  latticesize {mustBeNumeric}
-      latticenum {mustBeNumeric}
-      age {mustBeNumeric}
-   end
-   methods
-      function obj = Bug(row,col,lat)
-        obj.row = row;
-        obj.col = col;
-        obj.latticesize = lat;
-        obj.latticenum = lat*(row -1)+col;
-        obj.age = 0;
-      end
-      function obj = move(obj,validStandard)
+    properties
+        row {mustBeNumeric}
+        col {mustBeNumeric}
+        latticesize {mustBeNumeric}
+        latticenum {mustBeNumeric}
+        age {mustBeNumeric}
 
-	    r = rand;
-        if (r < validStandard)
-            moveDirection = randi(4);
-            if moveDirection == 1 && obj.row > 1
-                obj.row = obj.row - 1;
-                obj.col = obj.col;
-            elseif moveDirection == 2 && obj.row < obj.latticesize
-                obj.row = obj.row + 1;
-                obj.col = obj.col;
-            elseif moveDirection == 3 && obj.col > 1
-                obj.col = obj.col - 1;
-                obj.row = obj.row;
-            elseif moveDirection == 4 && obj.col < obj.latticesize
-                obj.col = obj.col + 1;
-                obj.row = obj.row;
-            else
-            	obj.row = obj.row;
-            	obj.col = obj.col;
-            end
-        else
-            obj.row = obj.row;
-            obj.col = obj.col;
+
+    end
+    methods
+        function obj = Bug(row,col,lat)
+            obj.row = row;
+            obj.col = col;
+            obj.latticesize = lat;
+            obj.latticenum = lat*(row -1)+col;
+            obj.age = 0;
         end
-      end
-   end
-end
+        function obj = move(obj,validStandard)
+            
+            r = rand;
+            if (r < validStandard)
+                moveDirection = randi(4);
+                if moveDirection == 1
+                    obj.row = obj.row - 1;
+                    obj.col = obj.col;
+                elseif moveDirection == 2
+                    obj.row = obj.row + 1;
+                    obj.col = obj.col;
+                elseif moveDirection == 3
+                    obj.col = obj.col - 1;
+                    obj.row = obj.row;
+                elseif moveDirection == 4
+                    obj.col = obj.col + 1;
+                    obj.row = obj.row;
+                else
+                    obj.row = obj.row;
+                    obj.col = obj.col;
+                end
+            else
+                obj.row = obj.row;
+                obj.col = obj.col;
+            end
+        end
 
+        function obj = grow(obj)
+            
+            obj.age = obj.age + 1;
+        end
+        
+        function obj = die(obj)
+            obj.row = NaN;
+            obj.col = NaN;
+        end
+    end
+end
